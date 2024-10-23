33012ae075fe80e70b77a234c84c1bb6815db4a5
JamesSupagit <supagit.ruarn@gmail.com>
2024-10-23 18:14:26 +0700

    update2

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,6 @@
 import { Container } from "./styles";
 import githubIcon from "../../assets/github.svg"
-import youtubeIcon from "../../assets/youtubeIcon.svg"
+//import youtubeIcon from "../../assets/youtubeIcon.svg"
 // import DownloadApp from '../../assets/download.png'
 import externalLink from "../../assets/external-link.svg"
 import ScrollAnimation from "react-animate-on-scroll";
